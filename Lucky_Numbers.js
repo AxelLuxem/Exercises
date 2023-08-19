be9a3d7b55e1d979a7081387d593fabc52e82380
AxelLuxem <104254539+AxelLuxem@users.noreply.github.com>
2023-08-20 00:37:05 +0100

    Create Lucky_Numbers.js

---
+++ Lucky_Numbers.js
@@ -0,0 +1,38 @@
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+
+export const twoSum = (array1, array2) => Number(array1.join("")) + Number(array2.join(""));
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean} whether the number is a palindrome or not
+ */
+
+export const luckyNumber = (value) => {
+  return value === Number(value.toString().split('').reverse().join(''));
+};
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+
+export const errorMessage = (input) => {
+  if (!input) {
+    return 'Required field';
+} else if (isNaN(+input) || +input === 0) {
+    return 'Must be a number besides 0';
+} else {
+    return '';
+}
+};
